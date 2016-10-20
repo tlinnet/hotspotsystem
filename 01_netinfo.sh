091e0e2bcd2c2c9b0fcb368b37456ef415f30dd5
Troels E. Linnet <tlinnet@gmail.com>
2016-10-20 03:12:32 +0200

    Added firewall info

--- 01_netinfo.sh
+++ 01_netinfo.sh
@@ -14,6 +14,8 @@ info() {
     echo -e "\nShow external IP"
     echo -e "Command: 'wget http://ipinfo.io/ip -qO -'"
     wget http://ipinfo.io/ip -qO -
+
+    echo "For firewall, see: https://www.digitalocean.com/community/tutorials/how-to-list-and-delete-iptables-firewall-rules"
 }
 
 # Perform
