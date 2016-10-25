8df6330c843714f5fa3057c6aeffab5814a88585
Troels E. Linnet <tlinnet@gmail.com>
2016-10-25 14:34:20 +0200

    Added a cronjob to check connection

---
+++ 05_cron_check_connection.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+
+# Get the external IP:
+EXTIP=`wget http://ipinfo.io/ip -qO -`
+SUCCESS=$?
+
+if [ "$SUCCESS" == "0" ]; then
+    echo "External connection returned IP: $EXTIP . And exit code: $SUCCESS"
+    logger -t connection "External connection returned IP: $EXTIP . And exit code: $SUCCESS"
+else
+    echo "No external connection found! Returned IP was: $EXTIP . And exit code: $SUCCESS"
+    echo "I will perform a 'reboot' now!!!"
+
+    logger -t connection "No external connection found! Returned IP was: $EXTIP . And exit code: $SUCCESS"
+    logger -t connection "I will perform a 'reboot' now!!!"
+
+    # Reboot
+    #/sbin/reboot
+fi
