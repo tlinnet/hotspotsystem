8177bdd63ff24f2445fe7706f99728d01d96d231
Troels E. Linnet <tlinnet@gmail.com>
2016-11-23 01:22:49 +0100

    Bug fix

--- api/pyt_vouchers.py
+++ api/pyt_vouchers.py
@@ -1,7 +1,7 @@
 import requests
 
 # The API key
-key = "fc8962e6f903acf14e3736f95a160b9f"
+key = "xx"
 
 # The location ID
 id = "4"
@@ -11,16 +11,24 @@ id = "4"
 headers = {'sn-apikey': key}
 # Root access
 root1 = 'https://api.hotspotsystem.com/v1.0'
-# Offset
-offset = 0
 # limit
 limit = 100
+# Offset
+offset = 0
+# sort
+#sort = "voucher_code"
+sort = None
 
-def api_locations_locationId_vouchers_v1(root=None, headers=None, id=None, offset=None, limit=None):
+def api_locations_locationId_vouchers_v1(root=None, headers=None, id=None, limit=None, offset=None, sort=None):
     url = root + '/locations/' + id + '/vouchers.json'
-    params = {'offset': offset, 'limit': limit}
+    params = {'limit': limit, 'offset': offset, 'sort': sort}
     r = requests.get(url, auth=(headers['sn-apikey'], 'x'), params=params)
+    #print r.json()
     for v in r.json()['results']:
-        print "C: %11s, V:% 5s m/H, P: %4s, T: %4s" % (v['voucher_code'], v['validity'], v['price_enduser'], v['usage_exp'])
+        #print "C: %11s, V:% 5s m/H, P: %4s, T: %4s" % (v['voucher_code'], v['validity'], v['price_enduser'], v['usage_exp'])
+        ctime = v['voucher_code'].split("-")[0]
+        if "p" in ctime:
+            continue
+        print "%11s, T: %4s" % (v['voucher_code'], v['usage_exp'])
 
-api_locations_locationId_vouchers_v1(root=root1, headers=headers, id=id, offset=offset, limit=limit)
+api_locations_locationId_vouchers_v1(root=root1, headers=headers, id=id, limit=limit, offset=offset, sort=sort)
