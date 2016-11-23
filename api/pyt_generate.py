7dc5fea0fb7f5bd7dd58e2065646af5caa6706e6
Troels E. Linnet <tlinnet@gmail.com>
2016-11-23 01:46:36 +0100

    Added generate voucher script

---
+++ api/pyt_generate.py
@@ -0,0 +1,14 @@
+import requests
+
+key = "x"
+
+i="4"
+h={'sn-apikey': key}
+root='https://api.hotspotsystem.com/v1.0'
+pac=7
+
+url=root+'/locations/'+i+'/generate/voucher.json'
+p = {'package': pac}
+r = requests.get(url, auth=(h['sn-apikey'], 'x'), params=p)
+#print r.json()['access_code']
+print r.json()
