6a71a48575c82d443d1079743b410ddc02a53100
Troels E. Linnet <tlinnet@gmail.com>
2016-11-23 01:53:23 +0100

    Made generate script

--- api/pyt_generate.py
+++ api/pyt_generate.py
@@ -10,5 +10,4 @@ pac=7
 url=root+'/locations/'+i+'/generate/voucher.json'
 p = {'package': pac}
 r = requests.get(url, auth=(h['sn-apikey'], 'x'), params=p)
-#print r.json()['access_code']
-print r.json()
+print r.json()['access_code']
