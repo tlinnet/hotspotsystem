5dba3d42506536d505fd9dd4fb17fbc7d51e0f12
Troels E. Linnet <tlinnet@gmail.com>
2016-11-25 23:08:37 +0100

    Change to turn off script

--- turn_off_led.sh
+++ turn_off_led.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #https://randomcoderdude.wordpress.com/2013/08/11/controlling-leds-status-in-openwrt/
 
-# Turn off trigger
+echo "Turn off trigger"
 for p in `find /sys/devices/platform/leds-gpio/leds -name trigger`; do
     echo $p
-    #echo none > $p
+    echo none > $p
 done
 
-# Set 0 as brightness
+echo ""
+echo "Set 0 as brightness"
 for p in `find /sys/devices/platform/leds-gpio/leds -name brightness`; do
     echo $p
-    #echo 0 > $p
+    echo 0 > $p
 done
