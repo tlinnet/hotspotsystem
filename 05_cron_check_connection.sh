d52eb4325cf1336450cd2b3c55d33633048d69dc
Troels E. Linnet <tlinnet@gmail.com>
2017-06-02 10:25:43 +0200

    Tried to do a reboot script

--- 05_cron_check_connection.sh
+++ 05_cron_check_connection.sh
@@ -7,6 +7,19 @@ SUCCESS=$?
 if [ "$SUCCESS" == "0" ]; then
     echo "External connection returned IP: $EXTIP . And exit code: $SUCCESS"
     logger -t connection "External connection returned IP: $EXTIP . And exit code: $SUCCESS"
+
+
+    # Check if it should reboot
+    # "10#$H" is the contents of the variable, in base 10.
+    H=$(date +%H)
+    if (( 8 <= 10#$H && 10#$H < 13 )); then
+        echo between 8AM and 1PM
+    elif (( 13 <= 10#$H && 10#$H < 23 )); then
+        echo between 1PM and 11PM
+    else
+        echo go to bed
+    fi
+
 else
     echo "No external connection found! Returned IP was: $EXTIP . And exit code: $SUCCESS"
     echo "I will perform a 'reboot' now!!!"
