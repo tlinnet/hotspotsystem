08ed99eca1b4319bad9e9ce25bed0110b8e6ee56
Troels E. Linnet <tlinnet@gmail.com>
2017-06-09 19:50:41 +0200

    Tried another nslookup command

--- 02_setup.sh
+++ 02_setup.sh
@@ -109,7 +109,7 @@ mkpapertrail() {
         PAPERURL=${PAPERURL:-$DEFPAPERURL}
         echo -e "You entered: $PAPERURL"
 
-        PAPERIP=`nslookup $PAPERURL | grep -m 1 "Address 1: " | cut -d" " -f3`
+        PAPERIP=`nslookup $PAPERURL | grep -m 1 "Address" | cut -d":" -f2 | sed 's/#.*//' | cut -d" " -f2 | tr -d " \t"`
         echo -e "\nWhen I do a nslookup, I translate the IP to: $PAPERIP"
 
         uci set system.@system[0].log_ip=$PAPERIP
